8420f4e7a3aef96f5756742ccf690f1463ab4f71
KobeMG <kobemoya@gmail.com>
2026-03-26 14:39:19 -0600

    Se agrega referencia a los buses de coronado.

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -6,7 +6,7 @@ const AdBanner = () => {
     <div className="ad-banner glass-card">
       <div className="ad-content">
         <span className="ad-tag">PRÓXIMAMENTE</span>
-        <p className="ad-text">¡Implementarémos buses externos, estén atentos!</p>
+        <p className="ad-text">¡Implementarémos buses externos, estén atentos! Puede consultar los de Coronado <a target="_blank" href="https://bus-ucr-coronado.netlify.app/">AQUI</a></p>
       </div>
     </div>
   );
